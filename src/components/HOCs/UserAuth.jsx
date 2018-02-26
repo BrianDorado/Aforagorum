f957d352f4d593cc0211acd93330f040f92b6337
Brian Larson <manateeman24@hotmail.com>
2018-02-26 14:03:52 -0700

    HOC skeleton

--- src/components/HOCs/UserAuth.jsx
+++ src/components/HOCs/UserAuth.jsx
@@ -1,1 +1,10 @@
-//  User Authentication 
+import {React, Component} from 'react'
+
+export default function(ComposedComponent){
+    class Authentication extends Component{
+        render(){
+            return <ComposedComponent {...this.props} /> 
+        }
+    }
+    return Authentication 
+}
--- src/components/landingPage/landingPage.jsx
+++ src/components/landingPage/landingPage.jsx
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import './landingPage.css';
 
 class LandingPage extends Component {
+  componentDidMount(){
+    console.log(process.env.REACT_APP_LOGIN);
+
+  }
   render() {
     return (
       <div className="landingpage-container">
